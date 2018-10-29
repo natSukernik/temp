631c1e20a6b21a18a9bb3736ee08259c931c813a
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2018-10-29 12:45:32 +0200

    added notification xml and activity

--- app/src/main/java/com/example/natsukernik/noforget/MainActivity.java
+++ app/src/main/java/com/example/natsukernik/noforget/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
 
