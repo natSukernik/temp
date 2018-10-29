c70045b041f31ff20f412011f7b1941544d9dcdb
ZedLeppelin <tamir.nahum@mail.huji.ac.il>
2018-10-29 12:57:37 +0200

    trying new notifications

--- app/src/main/java/com/example/natsukernik/noforget/MainActivity.java
+++ app/src/main/java/com/example/natsukernik/noforget/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
@@ -15,30 +16,43 @@ public class MainActivity extends AppCompatActivity {
 
     NotificationCompat.Builder notification;
     private static final int uniqueID = 45612;
-
+    private NotificationCompat.Builder mBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        R.id.bucksyButton.
 
-        notification = new NotificationCompat.Builder(this);
-        notification.setAutoCancel(true);
+//        notification = new NotificationCompat.Builder(this);
+//        notification.setAutoCancel(true);
+        mBuilder = new NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
     }
 
     public void bucksyButtonClicked(View view)
     {
-        notification.setSmallIcon(R.drawable.notification_icon);
-        notification.setTicker("This is the ticker");
-        notification.setWhen(System.currentTimeMillis());
-        notification.setContentTitle("Time to go shopping!");
-        notification.setContentText("We'll let you know what exactly you need to buy later");
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        notification.setContentIntent(pendingIntent);
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(2, mBuilder.build());
 
-        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        nm.notify(uniqueID, notification.build());
+//        notification.setSmallIcon(R.drawable.notification_icon);
+//        notification.setTicker("This is the ticker");
+//        notification.setWhen(System.currentTimeMillis());
+//        notification.setContentTitle("Time to go shopping!");
+//        notification.setContentText("We'll let you know what exactly you need to buy later");
+//
+//        Intent intent = new Intent(this, MainActivity.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        notification.setContentIntent(pendingIntent);
+//
+//        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        nm.notify(uniqueID, notification.build());
     }
 }
